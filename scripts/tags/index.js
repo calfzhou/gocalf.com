48f654288444cae4cec3b7ef276f877be22a44d9
Ji ZHOU <calf.zhou@gmail.com>
2024-04-23 22:20:35 +0800

    chore(hexo): add anim-cube related plugin and tag plugin

--- scripts/tags/index.js
+++ scripts/tags/index.js
@@ -22,3 +22,66 @@ hexo.extend.tag.register('badge_github', function (args) {
   }
   return '<p>' + parts.join(' ') + '</p>'
 })
+
+hexo.extend.tag.register('load_animcube', function (args) {
+  args = hexo.args.map(args, ['size'])
+  const { size = '3' } = args
+  return `<script src="/js/plugins/AnimCube${size}.js"></script>`
+})
+
+hexo.extend.tag.register('animcube', function (args) {
+  const cubeParamKeys = [
+    'align',
+    'bgcolor',
+    'borderwidth',
+    'butbgcolor',
+    'buttonbar',
+    'buttonheight',
+    'clickprogress',
+    'colors',
+    'colorscheme',
+    'config',
+    'counter',
+    'cubecolor',
+    'demo',
+    'doublespeed',
+    'edit',
+    'facelets',
+    'fonttype',
+    'gabbacolors',
+    'hint',
+    'hintborder',
+    'hinthoriz',
+    'hintvert',
+    'initmove',
+    'initrevmove',
+    'metric',
+    'move',
+    'movetext',
+    'movetextspace',
+    'perspective',
+    'pos',
+    'position',
+    'randmoves',
+    'repeat',
+    'scale',
+    'scramble',
+    'scw',
+    'sign',
+    'slidercolor',
+    'snap',
+    'speed',
+    'supercube',
+    'superfacelets',
+    'textsize',
+    'troughcolor',
+    'wca',
+    'yz',
+  ]
+  args = hexo.args.map(args, ['size', 'width', 'height', ...cubeParamKeys])
+  const { size = '3', width = '300px', height = '319px', others, ...cubeParams } = args
+  const control = Object.keys(cubeParams).map(key => `${key}=${cubeParams[key]}`).join('&')
+  const id = 'animcube-div-' + Math.random().toString(36).substring(2)
+  return `<div id="${id}" style="width: ${width}; height: ${height}"><script>window.addEventListener('load', function(){ AnimCube${size}("id=${id}&${control}") })</script></div>`
+  // return `<div style="width: ${width}; height: ${height}"><script>AnimCube${size}("${control}")</script></div>`
+})
