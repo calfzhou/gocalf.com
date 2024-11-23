233941065f145e286c3fd5d314ab09757bc61fa7
Calf <calf.zhou@gmail.com>
2024-11-23 21:49:13 +0800

    add(coding/102): binary-tree-level-order-traversal

---
+++ source/coding/102-binary-tree-level-order-traversal/solution.py
@@ -0,0 +1,31 @@
+from collections import deque
+from typing import Optional
+
+import sys
+sys.path.append('..')
+from _utils.binary_tree import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        values = []
+        queue = deque()
+        queue.append((root, 0))
+        while len(queue) > 0:
+            root, level = queue.popleft()
+            if root is None: continue
+
+            if level >= len(values):
+                values.append([])
+            values[-1].append(root.val)
+
+            queue.append((root.left, level + 1))
+            queue.append((root.right, level + 1))
+
+        return values
---
+++ source/coding/102-binary-tree-level-order-traversal/solution_test.py
@@ -0,0 +1,19 @@
+import pytest
+
+import sys
+sys.path.append('..')
+from _utils.binary_tree import build_tree
+from solution import Solution
+
+null = None
+
+
+@pytest.mark.parametrize('root, expected', [
+    ([3,9,20,null,null,15,7], [[3],[9,20],[15,7]]),
+    ([1], [[1]]),
+    ([], []),
+])
+class Test:
+    def test_solution(self, root, expected):
+        sol = Solution()
+        assert sol.levelOrder(build_tree(root)) == expected
