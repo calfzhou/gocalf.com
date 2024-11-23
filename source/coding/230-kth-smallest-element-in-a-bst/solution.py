e96a96f00ccc2def1b03a4b0bb32bfe7a3410b47
Calf <calf.zhou@gmail.com>
2024-11-23 22:14:00 +0800

    add(coding/230): kth-smallest-element-in-a-bst

---
+++ source/coding/230-kth-smallest-element-in-a-bst/solution.py
@@ -0,0 +1,27 @@
+from collections import deque
+from typing import Optional
+
+import sys
+sys.path.append('..')
+from _utils.binary_tree import TreeNode
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        i = 0
+        stack = []
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                if (i := i + 1) == k:
+                    return root.val
+                root = root.right
---
+++ source/coding/230-kth-smallest-element-in-a-bst/solution_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+import sys
+sys.path.append('..')
+from _utils.binary_tree import build_tree
+from solution import Solution
+
+null = None
+
+
+@pytest.mark.parametrize('root, k, expected', [
+    ([3,1,4,null,2], 1, 1),
+    ([5,3,6,2,4,null,null,1], 3, 3),
+])
+class Test:
+    def test_solution(self, root, k, expected):
+        sol = Solution()
+        assert sol.kthSmallest(build_tree(root), k) == expected
