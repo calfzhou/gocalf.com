7342d667d60e3f531eabc0bc4d9801d08f731f30
Calf <calf.zhou@gmail.com>
2024-12-18 11:32:47 +0800

    feat(scaffolds): update coding notes test code template

--- scaffolds/coding/solution_test.py
+++ scaffolds/coding/solution_test.py
@@ -7,7 +7,7 @@ from solution import Solution
     (0, 0),
     (1, 1),
 ])
+@pytest.mark.parametrize('sol', [Solution()])
 class Test:
-    def test_solution(self, param, expected):
-        sol = Solution()
+    def test_solution(self, sol, param, expected):
         assert sol.foo(param) == expected
