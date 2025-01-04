e390ebf001cbf3589a500254dfdd0b692b075c66
Calf <calf.zhou@gmail.com>
2025-01-04 23:01:25 +0800

    update(coding/3395): minor refine

--- source/coding/3395-subsequences-with-a-unique-middle-mode-i/solution.py
+++ source/coding/3395-subsequences-with-a-unique-middle-mode-i/solution.py
@@ -18,21 +18,21 @@ class Solution:
             right_counts[a] -= 1
 
             if l > 1:
-                al = left_counts[a]
-                ar = right_counts[a]
+                la = left_counts[a]
+                ra = right_counts[a]
                 cnt = nC2(l) * nC2(r)
-                cnt -= nC2(l - al) * nC2(r - ar)
+                cnt -= nC2(l - la) * nC2(r - ra)
                 for b in right_counts:
                     if b == a: continue
-                    bl = left_counts[b]
-                    br = right_counts[b]
-                    if bl + br < 2: continue
-                    if al > 0:
-                        cnt -= al * bl * br * (r - ar - br)
-                        cnt -= al * (l - al) * nC2(br)
-                    if ar > 0:
-                        cnt -= ar * bl * br * (l - al - bl)
-                        cnt -= ar * (r - ar) * nC2(bl)
+                    lb = left_counts[b]
+                    rb = right_counts[b]
+                    if lb + rb < 2: continue
+                    if la > 0:
+                        cnt -= la * lb * rb * (r - ra - rb)
+                        cnt -= la * (l - la) * nC2(rb)
+                    if ra > 0:
+                        cnt -= ra * lb * rb * (l - la - lb)
+                        cnt -= ra * (r - ra) * nC2(lb)
                 total = (total + cnt) % MOD
 
             left_counts[a] += 1
