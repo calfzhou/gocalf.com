4b7b65a98a61d743842d938bba79af112a8f4401
Calf <calf.zhou@gmail.com>
2025-12-27 01:52:36 +0800

    refactor(scripts): move some tag plugins to separated files

--- scripts/tags/index.js
+++ scripts/tags/index.js
@@ -23,77 +23,8 @@ hexo.extend.tag.register('badge_github', function (args) {
   return '<p>' + parts.join(' ') + '</p>'
 })
 
-hexo.extend.tag.register('animcube', function (args) {
-  const cubeParamKeys = [
-    'align',
-    'bgcolor',
-    'borderwidth',
-    'butbgcolor',
-    'buttonbar',
-    'buttonheight',
-    'clickprogress',
-    'colors',
-    'colorscheme',
-    'config',
-    'counter',
-    'cubecolor',
-    'demo',
-    'doublespeed',
-    'edit',
-    'facelets',
-    'fonttype',
-    'gabbacolors',
-    'hint',
-    'hintborder',
-    'hinthoriz',
-    'hintvert',
-    'initmove',
-    'initrevmove',
-    'metric',
-    'move',
-    'movetext',
-    'movetextspace',
-    'perspective',
-    'pos',
-    'position',
-    'randmoves',
-    'repeat',
-    'scale',
-    'scramble',
-    'scw',
-    'sign',
-    'slidercolor',
-    'snap',
-    'speed',
-    'supercube',
-    'superfacelets',
-    'textsize',
-    'troughcolor',
-    'wca',
-    'yz',
-    // My extra parameters.
-    'markers',
-  ]
-  args = hexo.args.map(args, ['size', 'width', 'height', ...cubeParamKeys])
-  const { size = '3', width = '300px', height = '319px', others, ...cubeParams } = args
-  const control = Object.keys(cubeParams).map(key => `${key}=${cubeParams[key]}`).join('&')
-  const id = 'animcube-div-' + Math.random().toString(36).substring(2)
-  return `<div id="${id}" style="width: ${width}; height: ${height}"><script>window.addEventListener('load', function(){ AnimCube${size}("id=${id}&${control}") })</script></div>`
-  // return `<div style="width: ${width}; height: ${height}"><script>AnimCube${size}("${control}")</script></div>`
-})
+hexo.extend.tag.register('animcube', require('./animate_cube')(hexo))
 
-hexo.extend.tag.register('invert', function (args, content) {
-  args = hexo.args.map(args, ['when'])
-  const { when = 'dark' } = args
-  const inner = hexo.render.renderSync({ text: content, engine: 'markdown' }) //.split('\n').join('')
-  const classes = []
-  if (when === 'dark' || when === 'always') {
-    classes.push('invert-when-dark')
-  }
-  if (when === 'light' || when === 'always') {
-    classes.push('invert-when-light')
-  }
-  return `<div class="${classes.join(' ')}">${inner}</div>`
-}, true)
+hexo.extend.tag.register('invert', require('./invert')(hexo), true)
 
 hexo.extend.tag.register('asset_code', require('./asset_code')(hexo), { async: true })
